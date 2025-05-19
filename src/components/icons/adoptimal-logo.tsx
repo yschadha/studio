9800737b380b6bd162448c561ab24ee7fef7dbca
Yuvraj Chadha <yschadha@gmail.com>
2025-05-19 09:52:00 +0000

    Use this one instead

--- src/components/icons/adoptimal-logo.tsx
+++ src/components/icons/adoptimal-logo.tsx
@@ -5,11 +5,11 @@ export function AdOptimalLogo(props: SVGProps<SVGSVGElement>) {
     <svg
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
+      {...props} // fill="currentColor" will be passed if set on the component, or path can have it directly
     >
       <path
         d="M12 0C12 0 11.25 11.25 0 12C11.25 12.75 12 24 12 24C12 24 12.75 12.75 24 12C12.75 11.25 12 0 12 0Z"
-        fill="currentColor"
+        fill="currentColor" // Ensures the path itself uses the current text color
       />
     </svg>
   );
